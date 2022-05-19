8a8f8a29ebaef5c7df26105999cc7392dbcad8d4
Simran <simran.ap@affordplan.com>
2022-05-19 11:40:21 +0530

    ADDED

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-app';
+  name = 'Simran';
+  contacts = [
+    {
+       'name' : 'ABC',
+       'id' : 123456789,
+       'city' : 'Rampur'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    },
+    {
+      'name' : 'XYZ',
+       'id' : 123456789,
+       'city' : 'Shimla'
+    }
+   
+  ]
+  userLoggedIn = true;
+  color = 'yellow';
+  clickFunction() {
+    console.log("hello, you clicked the button :)")
+  }
+  hoverFunction() {
+    console.log("hello, you just hovered me :)")
+  }
+
+ username = ""
+ lowerText = "I AM SIMRAN SINGLA"
+ upperText = "i am simran singla"
+ dateToday = Date.now()
+ curr = 100
+ percentVal = 0.292929
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
